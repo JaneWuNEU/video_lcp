1c217004a1a9c93cb2a597e5aa7ad6aaeb7d6178
rcwinder <r.c.winder@vu.nl>
2019-10-22 22:07:18 +0200

    basic_pipeline
    detection performs basic detection on an image
    server + client is basic pipeline.
    Client captures image from video/camera, sends it to server, waits for
    response and renders the bounding boxes
    Server does object detection on the received frame, and responds with
    the location of the objects

---
+++ CMakeFiles/CapDet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CapDet.dir/CapDet.cpp.o"
+  "CapDet.pdb"
+  "CapDet"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CapDet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/DetectionShort.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DetectionShort.dir/detectionShort.cpp.o"
+  "DetectionShort.pdb"
+  "DetectionShort"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DetectionShort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
