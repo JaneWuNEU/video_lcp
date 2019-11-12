4641b0e62eb29cff324dc8fcf5fc924877157442
rcwinder <r.c.winder@vu.nl>
2019-11-12 23:45:09 +0100

    darknetlib change

--- src/test_detection_darknet2.cpp
+++ src/test_detection_darknet2.cpp
@@ -14,10 +14,7 @@
 // It makes sense only for video-Camera (not for video-File)
 // To use - uncomment the following line. Optical-flow is supported only by OpenCV 3.x - 4.x
 //#define TRACK_OPTFLOW
-#define GPU
-#define OPENCV
-#define CUDNN
-#define CUDNN_HALF
+//#define GPU
 
 // To use 3D-stereo camera ZED - uncomment the following line. ZED_SDK should be installed.
 //#define ZED_STEREO
