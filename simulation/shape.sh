563cb20f4f974c680a81b92a346a724a2e3eca81
rcwinder <r.c.winder@vu.nl>
2020-02-16 22:59:54 +0100

    simulation tests

--- simulation/shape.sh
+++ simulation/shape.sh
@@ -12,13 +12,15 @@ sudo tc qdisc del dev $tc_adapter root
 sudo tc qdisc add dev $tc_adapter root handle 1: htb default 11
 sudo tc class add dev $tc_adapter parent 1: classid 1:11 htb rate 10mbit
 
+#iperf3 -c netmsys.org -p 10001 -t 1000 &
+
 for item in ${list[@]}
 do
 	if [ -z "${item//[$'\t\r\n ']}" ] #skip empty var
  	then 
 		continue
 	fi
-	date +"%H:%M:%S.%N BW set to ${item//[$'\t\r\n ']} mbit"
+	date +"B | %H:%M:%S.%N | $(printf "%.3f" ${item//[$'\t\r\n ']}) mbit"
 	
 	
 	sudo tc class change dev $tc_adapter parent 1: classid 1:11 htb rate "${item//[$'\t\r\n ']}"mbit
