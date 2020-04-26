87161be48a7b83d12ff7c3f433fed5aa84d3a0a2
rcwinder <r.c.winder@vu.nl>
2020-04-26 17:05:23 +0200

    grayscale

--- src/client_diff.cpp
+++ src/client_diff.cpp
@@ -371,7 +371,8 @@ void *capsend(void *fd) {
 			//resize and encode frame, send the size of the encoded frame so the server knows how much to read, and then send the data vector 
 			
 			auto e1 = getTickCount();
-
+			
+			cvtColor(local_frame_obj.frame, local_frame_obj.frame, COLOR_RGBA2GRAY);
 			resize(local_frame_obj.frame, local_frame_obj.frame, cv::Size(n_width[local_frame_obj.correct_model],n_height[local_frame_obj.correct_model]), 1, 1, cv::INTER_NEAREST);
 			imencode(".jpg", local_frame_obj.frame, vec);
 			size_t n = vec.size();
