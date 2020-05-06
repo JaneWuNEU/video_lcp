1658e5c515c6b15768a3a3bd16996fe70f76d899
rcwinder <r.c.winder@vu.nl>
2020-05-06 19:24:09 +0200

    server starting model switch

--- src/server.cpp
+++ src/server.cpp
@@ -396,7 +396,9 @@ int main(int argc, char *argv[]) {
 	
 	while(true){
 		if(argc == 3){
-			curr_model = atoi(argv[2]);
+			printf("please provide starting model\n");
+			curr_model = cin.get() - 48;
+			printf("%d starting model\n",curr_model);
 		} else {
 			curr_model = STARTING_MODEL;
 		}
